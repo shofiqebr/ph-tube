5f4c10cb4b37686af2d42b65edc36e1a9fd7fc08
shofiq <shofiqebr@gmail.com>
2023-09-01 22:53:20 +0600

    added category and card part

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const handleCatagory = async () =>{
     // console.log("ggggJ")
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json();
+    const tabContainer = document.getElementById("tab-container");
+
+        data.data.forEach((category) => {
+            const div = document.createElement('div');
+            div.innerHTML =`
+            <a onclick="handleId('${category.category_id}')" class="tab">${category.category}</a> 
+            `;
+            tabContainer.appendChild(div);
+        // console.log(category);
+    });
+
+ 
+ 
     console.log(data.data);
-}
+};
+const handleId = async (categoryId) => {
+    const res = await fetch(`
+    https://openapi.programming-hero.com/api/videos/category/${categoryId}
+    `);
+    const data = await res.json();
+    data.data.forEach((card) => {
+        console.log("card");
+    }); 
+    console.log(data);
+};
 handleCatagory();
