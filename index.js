b5297c8940b3130a30ba23d9f623689996679755
shofiq <shofiqebr@gmail.com>
2023-09-02 23:13:53 +0600

    added final site

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// load data
 const handleCatagory = async () => {
-  // console.log("ggggJ")
+
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
@@ -13,47 +12,49 @@ const handleCatagory = async () => {
             <a onclick="handleId('${category.category_id}')" class="tab">${category.category}</a> 
             `;
     tabContainer.appendChild(div);
-    // console.log(category.category);
+  
   });
 
-  // console.log(data);
 };
 const handleId = async (categoryId) => {
-//   console.log(categoryId);
+  
   const res = await fetch(`
     https://openapi.programming-hero.com/api/videos/category/${categoryId}
     `);
+   
+
   const data = await res.json();
 
   const cardContainer = document.getElementById("card-container");
   const cardContainer1 = document.getElementById("card-container1");
-
+  
   cardContainer.innerHTML = "";
   cardContainer1.innerHTML = "";
-  if (data.data && data.data.length > 0) {
-    data.data.forEach((card) => {
-        console.log(card.others);
+  if (data?.data && data?.data?.length > 0) {
+    data?.data.forEach((card) => {
+      
       const div = document.createElement("div");
       div.innerHTML = `
         <div class="card card-compact bg-base-100 shadow-xl">
-  <figure><img class="h-40 w-72" src="${card?.thumbnail}" alt="Shoes"/>
+  <figure><img class="h-40 w-72" src="${card?.thumbnail}" alt=""/>
   </figure>
   ${
     card?.others?.posted_date
-      ? `<p class="absolute bottom-36 bg-black rounded-md text-white right-2 text-sm text-gray-500">${getHoursAndMinutes(card?.others?.posted_date)}</p>`
+      ? `<p class="absolute bottom-36 md:right-16 right-7 bg-black rounded-md text-white lg:right-2 text-sm">${getHoursAndMinutes(card?.others?.posted_date)}</p>`
       : ''
   }
   <div class="card-description flex">
     <div class="img">
-    <img class="w-12 rounded-full m-5" src="${card?.authors[0]?.profile_picture}" alt="">
+    <img class="w-12 h-12 rounded-full m-5" src="${card?.authors[0]?.profile_picture}" alt="">
+
     
     </div>
   <div class="card-body">
-    <h2 class="card-title text-lg pt-4">${card?.title.slice(0,18)}</h2>
-    <p class="flex">${card?.authors[0]?.profile_name} ${
+    <h2 class="card-title text-lg pt-4">${card?.title.slice(0,10)}</h2>
+    <p class="flex">${card?.authors[0]?.profile_name.slice(0,12)} ${
       card?.authors[0]?.verified
         ? 
-        '<img class="w-8" src="https://logowik.com/content/uploads/images/check-mark-badge6475.logowik.com.webp" alt="Verified Badge" width="10" height="8">'
+        '<img class="w-8" src="https://logowik.com/content/uploads/images/check-mark-badge6475.logowik.com.webp" alt="Verified Badge">'
         : ''
     }
     </p>
@@ -90,16 +91,6 @@ function getHoursAndMinutes(durationInSeconds) {
 
   return `${hours}hrs ${minutes}min ago`;
 }
- 
-// console.log(data);
-
-
-
-
-
-
-
-
 
 handleId("1000");
 handleCatagory();
